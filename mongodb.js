f390bea8355414b99ee6e2dac285a8f727075d48
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-05 15:22:47 -0700

    learning update operation

--- mongodb.js
+++ mongodb.js
@@ -11,40 +11,34 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     console.log("connected succesfully")
     const db = client.db(databaseName)
 
-    // read data
-    // ******* findOne **********
-    // db.collection("users").findOne({name: "Jen"}, (error, user) => {
-    //     if(error) {
-    //         return console.log("unable to fetch")
-    //     }
-    //     console.log(user)
-    // })
+    // update data
+    // *********** updateOne ***********
+    db.collection("users").updateOne({
+        _id: new ObjectID("6366ce18e78139f0cae8c7a6")
+    },
+    {
+        $set: {
+            name: "Mike"
+        }
+    }).then((result) => {
+        console.log(result)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-    // ******* find ***********
-    // db.collection("users").find({age: "18"}).toArray((error, users) => {
-    //     console.log(users)
-    // })
-    // db.collection("users").find({age: "18"}).count((error, count) => {
-    //     console.log(count)
-    // })
-
-
-
-
-    // challenge
-    // ***** findone *****
-    // db.collection("tasks").findOne({_id: new ObjectID("6366c86cacd2c76dbd2906c0")}, (error, task) => {
-    //     if(error) {
-    //         return console.log("unable to fetch")
-    //     }
-    //     console.log(task)
-    // })
-
-    // ***** find multiple *****
-    // db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
-    //     console.log(tasks)
-    // })
-    // db.collection("tasks").find({completed: false}).count((error, count) => {
-    //     console.log(count)
-    // })
+    // ********** update multiple **********
+    db.collection("tasks").updateMany({
+        completed: false
+    },
+    {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 })
