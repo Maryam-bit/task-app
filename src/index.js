e0baca173ac64b78f0145414907c5fa085f263b2
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-12 22:10:26 +0500

    removed unused code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require("express")
 require("./db/mongoose")
-const User = require("./models/user");
-const Task = require("./models/task");
 
 
 const userRouter = require("./routers/user")
@@ -11,8 +9,6 @@ const port = process.env.PORT || 3000
 
 // automatically parse incomming json into an object
 app.use(express.json())
-
-
 app.use(userRouter)
 app.use(taskRouter)
 
