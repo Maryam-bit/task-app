b191eaf2f103a38aee97733d1f88002e184c8238
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-15 08:29:02 +0500

    sorting of tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,20 +19,29 @@ router.post("/tasks", auth, async (req, res) => {
 
 // GET /tasks?completed=true|false
 // GET /tasks?limit-10&skip=10
+// GET /tasks?soryBy=createdAt:asc
 router.get("/tasks", auth, async (req, res) => {
     try {
         //two ways
         // const tasks = await Task.find({ owner: req.user._id })
         const match = {}
+        const sort = {}
+        
         if(req.query.completed) {
             match.completed = req.query.completed == "true"
+        }
+
+        if(req.query.sortBy) {
+            const parts = req.query.sortBy.split(":")
+            sort[parts[0]] = parts[1] === "desc" ? -1 : 1
         }
         await req.user.populate({
             path: "tasks",
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         })
         // await req.user.populate('tasks')
