01279ee002248d8d2c1e60a5db0e683d0072d47f
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-16 00:16:02 +0500

    wip: router for upload images

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require("express")
 const User = require("../models/user");
 const router = new express.Router()
 const auth = require("../middleware/auth")
+const multer = require("multer")
 
 router.post("/users", async (req, res) => {
     const user = new User(req.body)
@@ -79,5 +80,22 @@ router.delete("/users/me", auth, async (req, res) => {
     }
 })
 
+const upload = multer({
+    dest: "avatars",
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("please upload an image"))
+        }
+        cb(undefined, true)
+    }
+})
+router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
 
 module.exports = router
