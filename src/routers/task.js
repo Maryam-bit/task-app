27133af60fc55c1f68af7528b30ff86d72d64597
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-15 08:16:46 +0500

    pagnination added for tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,6 +18,7 @@ router.post("/tasks", auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true|false
+// GET /tasks?limit-10&skip=10
 router.get("/tasks", auth, async (req, res) => {
     try {
         //two ways
@@ -28,7 +29,11 @@ router.get("/tasks", auth, async (req, res) => {
         }
         await req.user.populate({
             path: "tasks",
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         })
         // await req.user.populate('tasks')
         res.send(req.user.tasks)
