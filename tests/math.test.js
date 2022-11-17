85ef9464c77dac275dcc8ebc7fb97fbc71db2bdc
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-18 02:03:18 +0500

    added jest

---
+++ tests/math.test.js
@@ -0,0 +1,18 @@
+test("Hello world", () => {
+
+})
+
+test("this should fail", () => {
+    throw new Error("Failure")
+})
+
+/* 
+Why jest?
+  - saves time
+  - Create reliable software
+  - Gives flexibility to developers
+    - Refactoring
+    - Collaborating
+    - Profiling
+  - Peace of mind
+  */
