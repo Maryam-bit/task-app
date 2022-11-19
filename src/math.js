58bf5252a84ee9792cfe16e8cdf1d5fd62c51ef7
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-19 23:22:44 +0500

    learning jest (phase 2)

--- src/math.js
+++ src/math.js
@@ -1,8 +1,14 @@
-const calculateTip = (total, tipPercent) => {
-    const tip = total * tipPercent;
-    return total + tip;
+const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent);
+
+const farenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8;
 }
 
+const celciusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32;
+}
 module.exports = {
-    calculateTip
+    calculateTip,
+    farenheitToCelsius,
+    celciusToFahrenheit
 }
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,23 @@
-const {calculateTip} = require("../src/math.js")
+const {calculateTip, farenheitToCelsius, celciusToFahrenheit} = require("../src/math.js")
+
 test("Should caclculate total with tip", () => {
   const total = calculateTip(10, .3)
   expect(total).toBe(13)
+})
+
+test("Should caclculate total with default tip", () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12.5)
+})
+
+test("Should convert 32 F to 0 C", () => {
+  const tempInCelcius = farenheitToCelsius(32)
+  expect(tempInCelcius).toBe(0)
+})
+
+test("Should convert 0 C to 32 F", () => {
+  const tempInCelcius = celciusToFahrenheit(0)
+  expect(tempInCelcius).toBe(32)
 })
 /* 
 Why jest?
