493b87b325aee96040e2d7a54d51ce17a8d9a0ef
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-19 23:36:50 +0500

    testing asynchronous code

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip, farenheitToCelsius, celciusToFahrenheit} = require("../src/math.js")
+const {calculateTip, farenheitToCelsius, celciusToFahrenheit, add} = require("../src/math.js")
 
 test("Should caclculate total with tip", () => {
   const total = calculateTip(10, .3)
@@ -18,4 +18,23 @@ test("Should convert 32 F to 0 C", () => {
 test("Should convert 0 C to 32 F", () => {
   const tempInCelcius = celciusToFahrenheit(0)
   expect(tempInCelcius).toBe(32)
+})
+
+test("Async test demo", (done) => {
+  setTimeout(() => {
+    expect(1).toBe(1);
+    done()
+  }, 2000);
+})
+
+test("Should add too numbers", (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5)
+    done()
+  })
+})
+
+test("Should add too numbers async/await", async () => {
+  const sum = await add(2, 3)
+  expect(sum).toBe(5)
 })
